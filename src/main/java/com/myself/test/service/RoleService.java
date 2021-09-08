4bd2555882303f228c8381462598f58a044ccbb7
毛攀攀(qd_maopanpan) <qd_maopanpan@qdingnet.cn>
2021-09-08 12:58:32 +0800

    test1

--- src/main/java/com/myself/test/service/RoleService.java
+++ src/main/java/com/myself/test/service/RoleService.java
@@ -12,4 +12,8 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class RoleService {
+
+    public void sayHello(String msg) {
+        System.out.println("test:".concat(msg));
+    }
 }
