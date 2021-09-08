baf349453ba72853b9c7dc5d48f8d51bc9cd4a79
毛攀攀(qd_maopanpan) <qd_maopanpan@qdingnet.cn>
2021-09-08 12:06:45 +0800

    测试点1

--- src/main/java/com/myself/test/TestApplication.java
+++ src/main/java/com/myself/test/TestApplication.java
@@ -1,5 +1,6 @@
 package com.myself.test;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,9 +14,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * @version 1.0.0
  */
 @SpringBootApplication
-public class TestApplication {
+public class TestApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("测试一");
     }
 }
