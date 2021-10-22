6df7445ea20fc2094c00a3b7ed07db0889e8f5ac
毛攀攀(qd_maopanpan) <qd_maopanpan@qdingnet.cn>
2021-10-22 16:39:39 +0800

    测试

--- src/main/java/com/myself/test/service/RoleService.java
+++ src/main/java/com/myself/test/service/RoleService.java
@@ -13,8 +13,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class RoleService {
 
+    public void sayHello2(String msg) {
+        System.out.println("test2:".concat(msg));
+    }
 
-    public void testB() {
+    public void sayHello222(String msg) {
+        System.out.println("test222:".concat(msg));
+    }
 
+    public void helloInfo(String msg) {
+        System.out.println("helloInfo:".concat(msg));
     }
 }
